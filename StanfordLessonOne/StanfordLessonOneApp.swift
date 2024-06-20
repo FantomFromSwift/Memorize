217f76849a9786a84fe86560ca5a4aeb08aa50a7
Fantom <ivanneporadnyj@gmail.com>
2024-06-20 08:06:11 +0300

    Initial Commit

---
+++ StanfordLessonOne/StanfordLessonOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordLessonOneApp.swift
+//  StanfordLessonOne
+//
+//  Created by Иван Непорадный on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct StanfordLessonOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
